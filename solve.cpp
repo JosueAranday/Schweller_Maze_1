9117b3ff50490a324b9994938e84931c0ed9eac7
JosueAranday <josue.aranday01@utrgv.edu>
2025-11-01 21:35:57 -0500

    Made the vertices only, no neighbors yet

--- solve.cpp
+++ solve.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include <unordered_set>
@@ -9,10 +10,37 @@
 using namespace std;
 
 string solve(string maze) {
+	unordered_map<char, Vertex*> vertexMap;
 	string solvedMaze = "";
+	int counter = 0;
+	int xCoor = 0;
+	int yCoor = 0;
+	int xWidth = 0;
+	int yHeight = 0;
+	while (counter < maze.size()) {		// Create vertices for each open space in the maze
+		char curr = maze[counter];
+		if (curr == ' ') {
+			vertexMap[curr] = new Vertex(yCoor, xCoor);
+			//solvedMaze += ' ';
+			xCoor++;
+		}
+		else if (curr == '\n') {
+			xWidth = xCoor;
+			yCoor++;
+			xCoor = 0;
+			//solvedMaze += curr;
+		}
+		else {
+			xCoor++;
+			//solvedMaze += curr;
+		}
+		counter++;
+	}
+	yHeight = yCoor;
 
+	//cout << "Map is width: " << xWidth << " and Height: " << yHeight << endl;
 
-
+	// Create edges between neighboring vertices
 
 	return solvedMaze;
 }
